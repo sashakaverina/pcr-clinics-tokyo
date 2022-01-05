0c215915b42664e14ab9d85b63e1fc070112afb8
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-01-05 18:17:59 +0900

    added new and create actions

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -4,9 +4,16 @@ class ClinicsController < ApplicationController
   end
 
   def new
+    @clinic = Clinic.new
   end
 
   def create
+    @clinic = Clinic.new(clinic_params)
+    if @clinic.save
+      redirect_to clinics_path
+    else 
+     render 'new'  
+    end
   end
 
 end
